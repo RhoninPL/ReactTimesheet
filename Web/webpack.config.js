2069e1e4ee8c215548d7dae6ef5c50a795711bf0
Michal Switalik <michal.switalik@gmail.com>
2017-03-09 20:19:44 +0100

    Project init

---
+++ Web/webpack.config.js
@@ -0,0 +1,20 @@
+﻿"use strict";
+
+module.exports = {
+    entry: "./wwwroot/js/index.js",
+    output: {
+        filename: "wwwroot/bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+                query: {
+                    presets: ["es2015", "react"]
+                }
+            }
+        ]
+    }
+};
